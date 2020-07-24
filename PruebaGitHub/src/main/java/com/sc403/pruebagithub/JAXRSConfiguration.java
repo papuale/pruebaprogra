955a602a481a34fe4585883eb312a2142e7d1449
papuale <lfallas60605@ufide.ac.cr>
2020-07-23 22:18:59 -0600

    Acaba de crear un nuevo proyecto, mi aporte esta en index.html

---
+++ PruebaGitHub/src/main/java/com/sc403/pruebagithub/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.sc403.pruebagithub;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
